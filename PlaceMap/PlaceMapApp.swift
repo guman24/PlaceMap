4902f2dd9cd08c741aa5bfd10af713f37f26720e
Rajan Karki <gomansinghkarki@gmail.com>
2024-02-03 07:53:32 +0545

    project init

---
+++ PlaceMap/PlaceMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceMapApp.swift
+//  PlaceMap
+//
+//  Created by Rajan Karki on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaceMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
